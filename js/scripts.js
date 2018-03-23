5007039e4367b47e6a15986f4bee393149ced424
Matt Dowdney <theofficialmatt@gmail.com>
2018-03-23 08:59:57 -0700

    declares initial jQuery variables

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,8 @@
 $(document).ready(function(){
-  
+  $("#survey").submit(function (event) {
+      var userArt = parseInt($("#art").val());
+      var userLeftRight = parseInt($("#leftright").val());
+      var userCareer = parseInt($("#career").val());
+      var userDislike = parseInt($("#dislike").val());
+      var userAffinity = parseInt($("#affinity").val());
 })
